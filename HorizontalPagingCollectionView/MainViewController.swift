06246371e965161b8cfe9281596705220f99b708
Artem Ekimov <tehman3d@mail.ru>
2023-03-08 08:47:35 +0400

    Add collection view (without paging)

--- HorizontalPagingCollectionView/MainViewController.swift
+++ HorizontalPagingCollectionView/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  HorizontalPagingCollectionView
 //
 //  Created by Artem Ekimov on 3/6/23.
@@ -8,12 +8,92 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    private struct Item {
+        let color: UIColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
+    
+    private lazy var layout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = .zero
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        return layout
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.register(UICollectionViewCell.self)
+        collection.showsHorizontalScrollIndicator = false
+        collection.decelerationRate = .fast
+        collection.backgroundColor = .clear
+        collection.delegate = self
+        collection.dataSource = self
+        return collection
+    }()
 
+    private let height: CGFloat = 250
 
+    private let items = [
+        Item(color: UIColor.random),
+        Item(color: UIColor.random),
+        Item(color: UIColor.random),
+        Item(color: UIColor.random),
+        Item(color: UIColor.random)
+    ]
+
+    private func configureUI() {
+        title = "Main"
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+            collectionView.heightAnchor.constraint(equalToConstant: height)
+        ])
+    }
 }
 
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+        let item = items[indexPath.row]
+        cell.backgroundColor = item.color
+        cell.layer.cornerRadius = 12
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: height)
+    }
+}
+
+
+extension UICollectionView {
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        dequeueReusableCell(withReuseIdentifier: String(describing: T.self), for: indexPath) as! T
+    }
+    
+    func register<T: UICollectionViewCell>(_ cell: T.Type) {
+        register(T.self, forCellWithReuseIdentifier:  String(describing: T.self))
+    }
+}
+
+extension UIColor {
+    static var random: UIColor {
+        UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1)
+    }
+}
